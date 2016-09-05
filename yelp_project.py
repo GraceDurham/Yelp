58fff81310860a071fa8595b344f722ab99edd21
Grace Durham <gracelee.durham@gmail.com>
2016-09-05 14:51:43 -0700

    Add files via upload

--- yelp_project.py
+++ yelp_project.py
@@ -1,12 +1,15 @@
 Choice=raw_input("Please enter a state")
 Choice1=raw_input("Please enter a city")
 
-use weather api to return current conditions for city, state 
-here are current weather conditions 
-Based on todays weather 
-use conditional if 60-80 suggest resteraunts with out door seating 
-else less than 60 suggest resetaunts with indoor seating 
-here are the results with out door seating for restaurants in sf or city, and state they choose
+# use weather api to return current conditions for city, state 
+# here are current weather conditions 
+# Based on todays weather 
+# use yelp api for resteraunts with out door seating 
+# use conditional if 60-80 suggest resteraunts with out door seating 
+# else less than 60 suggest resetaunts with indoor seating 
+# here are the results with out door seating for restaurants in sf or city, and state they choose
+
+# idea maybe create class contact with self, first_name, last_name, email="", mobile_phone="", text_message, 
 
 
 
